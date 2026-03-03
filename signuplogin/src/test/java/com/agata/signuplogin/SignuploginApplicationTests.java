ec3626b9d14ee18f5c29a385a72e7527fc8167a0
agata <agata.fernanda4@gmail.com>
2026-03-03 08:51:51 -0300

    Initial commit

---
+++ signuplogin/src/test/java/com/agata/signuplogin/SignuploginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agata.signuplogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignuploginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
