4f40028b106917f680b037e6f2761ce8e35454bd
agata <agata.fernanda4@gmail.com>
2026-03-04 09:08:31 -0300

    feat: Criar entidade User com nome, email, senha, tentativas e bloqueio

---
+++ signuplogin/src/main/java/com/agata/signuplogin/model/User.java
@@ -0,0 +1,86 @@
+package com.agata.signuplogin.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+    private int chances;
+    private boolean blocked;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, int chances, boolean blocked) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.chances = chances;
+        this.blocked = blocked;
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getChances() {
+        return chances;
+    }
+
+    public void setChances(int chances) {
+        this.chances = chances;
+    }
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
+}
